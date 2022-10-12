a5289176db23844b742752594263cab04a8168c5
lib <lib.martinito@pm.me>
2022-10-12 22:31:36 +0800

    added error handling for register

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,27 +1,56 @@
 <script setup lang="ts">
 import { store } from "@/store/store"
+
+type JSONResponse = {
+    user?: {
+        id: string,
+        email: string,
+        created_at: string,
+        updated_at: string
+    },
+    token?: string,
+    message?: string
+}
+
 const email = ref<string>("")
 const password = ref<string>("")
+const confirmPassword = ref<string>("")
+
+const arePasswordsDifferent = ref<boolean>(false)
+const isUserRegistered = ref<boolean>(false)
+
+const resetDefaultState = () => {
+    arePasswordsDifferent.value = false
+    isUserRegistered.value = false
+}
+
 const register = async () => {
-    interface Response {
-        user: {
-            id: string,
-            email: string,
-            created_at: string,
-            updated_at: string
-        },
-        token: string
+    resetDefaultState()
+    if (password.value !== confirmPassword.value) {
+        arePasswordsDifferent.value = true
+        return
     }
-    const url = "http://localhost:3333/api/user/register"
-    const options = {
+    const url: string = "http://localhost:3333/api/user/register"
+
+    const res: Response = await fetch(url, {
         method: 'POST',
-        body: {
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
             email: email.value,
             password: password.value
-        }
+        })
+    })
+    const jsonRes: JSONResponse = await res.json()
+
+    if (jsonRes.message) {
+        isUserRegistered.value = true
+        return
     }
-    const { data } = await useFetch<Response>(url, options)
-    store.updateUserInfo(data.value.user, data.value.token)
+
+    store.updateUserInfo(jsonRes.user, jsonRes.token)
     await navigateTo("/home")
 }
 </script>
@@ -29,8 +58,13 @@ const register = async () => {
 <template>
     <form @submit.prevent="register" class="register">
         <input type="email" placeholder="email" class="email" required v-model="email" />
+        <div class="error" v-if="isUserRegistered">email is already used. please log in.</div>
         <input type="password" placeholder="password" class="password" required v-model="password" />
-        <div class="login">already have an account?<NuxtLink to="/login">log in</NuxtLink></div>
+        <input type="password" placeholder="confirm password" class="confirm-password" required
+            v-model="confirmPassword" />
+        <div class="error" v-if="arePasswordsDifferent">passwords do not match. please try again.</div>
+        <div class="login">already have an account?<NuxtLink to="/login">log in</NuxtLink>
+        </div>
         <button type="submit" class="submit">register</button>
     </form>
 </template>
@@ -43,6 +77,7 @@ const register = async () => {
     min-height: 100vh;
     align-items: center;
     justify-content: center;
+
     input {
         width: 384px;
         box-sizing: border-box;
@@ -50,18 +85,22 @@ const register = async () => {
         padding: 4px 8px;
         border: 1px solid #525252;
     }
+
     input:hover {
         border: 1px solid #262626;
     }
+
     input:focus {
         outline: none;
         padding: 3px 7px;
         border: 2px solid #262626;
     }
+
     .login {
         font-size: 16px;
         padding-top: 16px;
     }
+
     button {
         width: 384px;
         padding: 4px 8px;
@@ -71,8 +110,14 @@ const register = async () => {
         line-height: 1.3;
         cursor: pointer;
     }
+
     button:hover {
         background-color: #262626;
+    }
+
+    .error {
+        color: #dc2626;
+        font-size: 16px;
     }
 }
 </style>
